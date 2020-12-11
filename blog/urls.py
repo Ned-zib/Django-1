6d2b3f0111d7169c4a211e52d586682acf86591c
Nedzib <nedzib.sastoque@gmail.com>
2020-12-10 20:00:30 -0500

    added Pagination and one class-based view

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     # Post views
-    path('', views.post_list, name='post_list'),
+    #path('', views.post_list, name='post_list'), #We only comment this to check the Class-Based view
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from blog.models import Post
-
+from django.views.generic import ListView
 # Getting all the published post
 
+# Class-Based Views
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    # All published posts
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 Post in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver the last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
     # Render(request, template path, context variables)
 
 # A view of one single post
@@ -14,12 +32,10 @@ def post_list(request):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,  slug=post,
-                                    status='published',
-                                    publish__year=year,
-                                    publish__month=month,
-                                    publish__day=day)
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
-
-        
